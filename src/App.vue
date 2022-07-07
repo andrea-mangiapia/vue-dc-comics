9c9c438a0a56c4332f8658dbb089978475863938
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-07 13:11:20 +0200

    add Single Card

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <PageHeader/>
 
     <!-- MAIN -->
-    <main>    
+    <main>   
+      <SectionCard/> 
       <SectionContentMain/>
       <SectionBuyList/>    
     </main>
@@ -17,14 +18,16 @@
 
 <script>
 import PageHeader from './components/PageHeader.vue';
-import PageFooter from './components/PageFooter.vue';
+import SectionCard from './components/SectionCard.vue';
 import SectionContentMain from './components/SectionContentMain.vue';
 import SectionBuyList from './components/SectionBuyList.vue';
+import PageFooter from './components/PageFooter.vue';
 
 export default {
   name: 'App',
   components: {
     PageHeader,
+    SectionCard,
     SectionContentMain,
     SectionBuyList,
     PageFooter
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- Single Card -->
+    <div class="single-card">
+        <!-- Image Card -->
+        <div class="img-card">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+        </div>
+        <!-- Title Series Card -->
+        <h2 class="series-card">
+            Action Comics
+        </h2>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'SingleCard',
+
+}
+</script>
+
+<style scoped lang="scss">
+
+.single-card {
+width: calc((100% / 6) - 20px);
+margin: 10px 10px 30px 10px;
+
+    .series-card {
+        margin-top: 10px;
+        font-size: 14px;
+    }
+}
+
+</style>
